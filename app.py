ac082c3deff814b8d1dacf383c922d9cde4e2868
Wentian Fang <fangwt96@gmail.com>
2019-07-09 22:31:00 +0100

    Test the ability of Python Backend

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import os
+import AIR
 
 app = Flask(__name__, static_url_path='')
 #app = Flask(__name__)
@@ -8,9 +9,14 @@ port = int(os.getenv('PORT', 8000))
 
 @app.route('/')
 def hello_world():
-    #return app.send_static_file('index.html')
+    return app.send_static_file('index.html')
     #print("Hello World")
-    return "Hello New World 2.0"
+    #return "Hello New World 2.0"
+
+@app.route("/test",methods=['POST','GET'])
+def test():
+    return AIR.test()
+
 
 
 if __name__ == '__main__':
