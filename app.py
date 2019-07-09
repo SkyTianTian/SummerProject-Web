13deaaa5a666222358ba877665f86157cad28b6d
Wentian Fang <fangwt96@gmail.com>
2019-07-09 14:53:39 +0100

    Updaate All

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os
+
+app = Flask(__name__, static_url_path='')
+#app = Flask(__name__)
+port = int(os.getenv('PORT', 8000))
+
+
+@app.route('/')
+def hello_world():
+    #return app.send_static_file('index.html')
+    #print("Hello World")
+    return "Hello World"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port, debug=False)
